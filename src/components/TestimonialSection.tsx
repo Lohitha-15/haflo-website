f17c589c9311f5d408ea4bc280081bb2d632cc13
Lohitha-15 <lohitha.kota@ensarsolutions.com>
2025-04-27 11:52:15 +0530

    Haflo Website

--- src/components/TestimonialSection.tsx
+++ src/components/TestimonialSection.tsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react';
+import { useState, useEffect } from 'react';
 
 const TestimonialSection = () => {
   const [activeIndex, setActiveIndex] = useState(0);
